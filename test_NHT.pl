d8a10ee53c8d8c8d32f232b66eb060260024d219
gamache <gamache@ftso.net>
2009-12-03 13:01:11 -0500

    post november 2009 commit

--- test_NHT.pl
+++ test_NHT.pl
@@ -10,11 +10,10 @@ use NHT;
 my $nht = NHT->new(xlogfile => 'scores.xlogfile.2009', debug => 1);
 
 
-for (qw//) { # qw/ birdie hat_trick /) { #($nht->trophies) {
-  print "$_ results:\n";
-  print Dumper [ @{$nht->trophy($_)}[0..4] ];
-}
+#for (qw//) { # qw/ birdie hat_trick /) { #($nht->trophies) {
+#  print "$_ results:\n";
+#  print Dumper [ @{$nht->trophy($_)}[0..4] ];
+#}
 
-print Dumper [ @{ $nht->trophy('underperformers') }[0..4] ];
-
+print Dumper [ @{ $nht->trophy('runaway') }[0..4] ];
 
