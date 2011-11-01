088e6720019b483d3c100438373fc0f04965d0ae
pete gamache <gamache@gmail.com>
2011-11-01 08:40:29 -0400

    stuff

--- update_scores_2010.pl
+++ update_scores_2010.pl
@@ -9,18 +9,18 @@ unless (-f '/tmp/eit2010.update') {
   system('touch /tmp/eit2010.update');
   
   my $this_pid   = $$;
-  my $server_pid = `cat /tmp/eit.pid.*`;
-  my @server_pids = split /\s+/, $server_pid;
+  my $server_pid = `cat /tmp/eit2010.pid.*`;
+  chomp $server_pid;
   
   chdir '/home/gamache/sites/EIT2010' or die $!;
   
-  system('lynx -source http://nethack.devnull.net/tournament/scores.xlogfile > scores.xlogfile.2010') unless $ARGV[0] eq '--no-dl';
+  #system('lynx -source http://nethack.devnull.net/tournament/scores.xlogfile > scores.xlogfile.2010');
    
   system("script/eit_fastcgi.pl -l /tmp/eit2010.socket.$this_pid -n 3 -p /tmp/eit2010.pid.$this_pid -d");
   
   system("ln -fs /tmp/eit2010.socket.$this_pid /tmp/eit2010.socket");
   
-  system("kill $_") for @server_pids;
+  system("kill $server_pid") if $server_pid;
   
   # clean up /tmp
   for (glob "/tmp/eit2010.socket.*") {
